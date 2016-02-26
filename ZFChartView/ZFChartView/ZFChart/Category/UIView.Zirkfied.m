ab4dea60e0a10ad5ce82f3d066b3eb6a6b877c95
Zirkfied <273987506@qq.com>
2016-02-26 11:40:09 +0800

    Added files via upload

---
+++ ZFChartView/ZFChartView/ZFChart/Category/UIView.Zirkfied.m
@@ -0,0 +1,26 @@
+//
+//  UIView+Zirkfied.m
+//  ZFChartView
+//
+//  Created by apple on 16/2/24.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "UIView+Zirkfied.h"
+
+@implementation UIView (Zirkfied)
+
+/**
+ *  自定义边框
+ *
+ *  @param cornerRadius 角落半径
+ *  @param borderWidth  边框宽度
+ *  @param color        边框颜色
+ */
+-(void)setBorderCornerRadius:(CGFloat)cornerRadius andBorderWidth:(CGFloat)borderWidth andBorderColor:(UIColor *)color{
+    self.layer.cornerRadius = cornerRadius;
+    self.layer.borderWidth = borderWidth;
+    self.layer.borderColor = color.CGColor;
+}
+
+@end
